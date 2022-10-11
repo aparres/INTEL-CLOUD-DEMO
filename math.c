b002969160429bfcc56d3e429fd28844047412d8
Alvaro Parres <aparres@gmail.com>
2022-10-11 10:01:51 -0500

    Math.c y Math.h

---
+++ math.c
@@ -0,0 +1,13 @@
+/* Archivos MAth.c */
+
+
+
+int doblEntero(int a) {
+    if(a > MAX_INT/2) {
+        return -1
+    } 
+    return a*2:
+}
+
+
+}
---
+++ math.h
@@ -0,0 +1,5 @@
+/* Encabesados de Math.h */
+
+#define MAX_INT pow(2,sizeof(int))/2
+
+int doblEntero(int a);
