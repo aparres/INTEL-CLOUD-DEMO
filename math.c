23aaf431b2b15b6c2f2a022f814590eec967052e
Alvaro Parres <aparres@gmail.com>
2022-10-11 10:31:00 -0500

    Nueva funcion

--- math.c
+++ math.c
@@ -4,7 +4,14 @@
 
 int doblEntero(int a) {
     if(a > MAX_INT/2) {
-        return -1
+        return -1;
+    } 
+    return a*2;
+}
+
+double dobleDouble(double a) {
+    if(a > MAX_DOUBLE/2) {
+        return -1;
     } 
     return a*2;
 }
