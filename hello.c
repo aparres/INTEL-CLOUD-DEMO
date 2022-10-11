7759b9626b007e549059c7ab27b0afc8c252e7b1
Alvaro Parres <aparres@gmail.com>
2022-10-11 10:09:23 -0500

    Create and use math files

--- hello.c
+++ hello.c
@@ -1,7 +1,9 @@
 
 #include <stdio.h>
+#include "math.h"
 
 int main(void) {
     printf("Hola Mundo!\n");
+    printf("El doble de 5 es: %i",doblEntero(5));
     return 0;
 }
--- math.c
+++ math.c
@@ -1,13 +1,10 @@
 /* Archivos MAth.c */
 
-
+#include "math.h"
 
 int doblEntero(int a) {
     if(a > MAX_INT/2) {
         return -1
     } 
-    return a*2:
-}
-
-
+    return a*2;
 }
